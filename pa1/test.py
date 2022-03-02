4374e60711d96ca8af28883e2a84d2282753cc88
Lukas Hering <lukasheringb@gmail.com>
2022-03-01 20:46:50 -0600

    add final pa1 code

--- pa1/test.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-if __name__ == '__main__':
-    img = cv2.imread('test.png')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(image=gray, threshold1=100, threshold2=200)
-    cv2.imwrite(filename='test_out.png', img=edges)
