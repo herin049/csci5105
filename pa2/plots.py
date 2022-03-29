1f0ef2b0dec4f79e13b8b8ec7054ae5172767242
Lukas Hering <lukasheringb@gmail.com>
2022-03-28 22:20:27 -0500

    add chart generation code

---
+++ pa2/plots.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+from numpy import genfromtxt
+import numpy as np
+
+if __name__ == '__main__':
+    execution_time = [2.7746 , 2.3825]
+    operations_per_sec = [180 / e for e in execution_time]
+    bars = ('Caching Disabled', 'Caching Enabled')
+    y_pos = np.arange(len(bars))
+
+    plt.bar(y_pos, operations_per_sec)
+    plt.ylabel('Operations per second')
+    plt.xticks(y_pos, bars)
+    
+    plt.show()
+    plt.savefig('caching_chart.png')
